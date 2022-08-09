118adef2862a95f4ad650df443bbb93e38f20d81
liubin <liubin@sensetime.com>
2022-08-09 17:15:19 +0800

    add loop method

---
+++ algorithm/src/main/java/com/strawbingo/algorithm/sword/ch4/BinaryTreePrint.java
@@ -0,0 +1,114 @@
+package com.strawbingo.algorithm.sword.ch4;
+
+import com.strawbingo.algorithm.sword.TreeNode;
+
+import java.util.*;
+
+/**
+ * 题32：从上到下打印二叉树
+ */
+public class BinaryTreePrint {
+
+    /**
+     * 题目一：不分行，从上到下打印二叉树
+     * 从上到下打印二叉树的每个节点，同一层的节点按照从左到右的顺序打印
+     *      3
+     *     / \
+     *    4   5
+     *   / \
+     *  1   2
+     */
+    public int[] levelOrder(TreeNode root) {
+        if(root == null) return new int[0];
+
+        List<Integer> arr = new ArrayList<>();
+        Queue<TreeNode> que = new LinkedList<>();
+        que.add(root);
+        while (!que.isEmpty()){
+            TreeNode node = que.poll();
+            if(node !=null) {
+                arr.add(node.getValue());
+                que.add(node.getLeftNode());
+                que.add(node.getRightNode());
+            }
+        }
+
+        int[] res = new int[arr.size()];
+        int i = 0;
+        for(int tmp:arr){
+            res[i++] = tmp;
+        }
+
+        return res;
+    }
+
+    /**
+     * 题目二：分行从上到下打印二叉树
+     * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行
+     */
+    public List<List<Integer>> levelOrderLineFeed(TreeNode root) {
+        if(root == null) return null;
+
+        Queue<TreeNode> que = new LinkedList<>();
+        Queue<TreeNode> queTmp = new LinkedList<>();
+        que.add(root);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        //que is not null
+        List<Integer> inner = new ArrayList<>();
+        while (!que.isEmpty()){
+            TreeNode node = que.poll();
+            inner.add(node.getValue());
+            if(node.getLeftNode()!= null) queTmp.add(node.getLeftNode());
+            if(node.getRightNode() !=null) queTmp.add(node.getRightNode());
+
+            //que is null and tmp is not null
+            if(que.isEmpty()){
+                res.add(inner);
+                inner = new ArrayList<>();
+                while (!queTmp.isEmpty())
+                    que.add(queTmp.poll());
+                }
+            }
+
+        return res;
+
+    }
+
+
+    /**
+     * 题目三：之字形打印二叉树
+     * 即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印
+     */
+    public List<List<Integer>> levelOrderZLineFeed(TreeNode root) {
+        if(root == null) return new ArrayList<>();
+        //
+
+        Queue<TreeNode> que = new ArrayDeque();
+        que.add(root);
+        boolean z = true;
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        //if stack is not null print order
+        while (!que.isEmpty()) {
+            int forSize = que.size();
+            //loop print
+            Deque<Integer> inner = new LinkedList<>();
+            for(int i=0; i<forSize; i++){
+                TreeNode node = que.poll();
+                if(z){
+                    inner.addLast(node.getValue());
+                }else {
+                    inner.addFirst(node.getValue());
+                }
+                if(node.getLeftNode() != null) que.add(node.getLeftNode());
+                if(node.getRightNode() != null)  que.add(node.getRightNode());
+            }
+            res.add(new ArrayList<>(inner));
+            z = !z;
+        }
+
+        return res;
+    }
+
+}
+
---
+++ algorithm/src/test/java/com/strawbingo/algorithm/sword/ch4/BinaryTreePrintTest.java
@@ -0,0 +1,77 @@
+package com.strawbingo.algorithm.sword.ch4;
+
+import com.strawbingo.algorithm.sword.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * 题32：从上到下打印二叉树
+ */
+public class BinaryTreePrintTest {
+
+    private TreeNode setUpParentTree(){
+        TreeNode root = new TreeNode(3);
+
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+
+        root.setLeftNode(node4);
+        root.setRightNode(node5);
+        node4.setLeftNode(node1);
+        node4.setRightNode(node2);
+
+        return root;
+    }
+
+    /**
+     * 题目一：不分行，从上到下打印二叉树
+     * 从上到下打印二叉树的每个节点，同一层的节点按照从左到右的顺序打印
+     *      3
+     *     / \
+     *    4   5
+     *   / \
+     *  1   2
+     */
+    @Test
+    public void testLevelOrder(){
+        BinaryTreePrint binaryTreePrint = new BinaryTreePrint();
+        TreeNode root = setUpParentTree();
+
+        int[] order = binaryTreePrint.levelOrder(root);
+        Assert.assertEquals(2,order[4]);
+        Assert.assertEquals(5,order[2]);
+    }
+
+    /**
+     * 题目二：分行从上到下打印二叉树
+     * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行
+     */
+    @Test
+    public void testLevelOrderLineFeed() {
+        BinaryTreePrint binaryTreePrint = new BinaryTreePrint();
+        TreeNode root = setUpParentTree();
+
+        List<List<Integer>>  order = binaryTreePrint.levelOrderLineFeed(root);
+        Assert.assertEquals(3,order.size());
+        Assert.assertEquals(2,order.get(2).size());
+    }
+
+    /**
+     * 题目三：之字形打印二叉树
+     * 即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印
+     */
+    @Test
+    public void testLevelOrderZLineFeed() {
+        BinaryTreePrint binaryTreePrint = new BinaryTreePrint();
+        TreeNode root = setUpParentTree();
+
+        List<List<Integer>>  order = binaryTreePrint.levelOrderZLineFeed(root);
+        Assert.assertEquals(3,order.size());
+        Assert.assertEquals(2,order.get(2).get(1).intValue());
+    }
+
+}
