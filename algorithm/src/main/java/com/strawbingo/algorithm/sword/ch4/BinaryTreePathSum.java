a65ea72f11e33447b1405f31f652ddeb87df14c4
liubin <liubin@sensetime.com>
2022-08-12 10:58:39 +0800

    remove redundancy code

--- algorithm/src/main/java/com/strawbingo/algorithm/sword/ch4/BinaryTreePathSum.java
+++ algorithm/src/main/java/com/strawbingo/algorithm/sword/ch4/BinaryTreePathSum.java
@@ -3,7 +3,6 @@ package com.strawbingo.algorithm.sword.ch4;
 import com.strawbingo.algorithm.sword.TreeNode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class BinaryTreePathSum {
@@ -20,32 +19,21 @@ public class BinaryTreePathSum {
     private void pathSumRecuration(TreeNode root, int target,
               List<Integer> intList, List<List<Integer>> res) {
 
-        //check left node ,if not do rec
-        if(root != null){
-//            Arrays.stream(intList.toArray()).forEach(t -> System.out.print(t+","));
-//            System.out.println("root:"+root.getValue());
-            int value = root.getValue();
-            //if == target ,add to result;
-            if(value == target && root.getLeftNode() ==null && root.getRightNode() ==null){
-
-//                System.out.println("match");
-                intList.add(value);
-//                Arrays.stream(intList.toArray()).forEach(t -> System.out.print(t + ","));
-                res.add(new ArrayList(intList));
-                intList.remove(intList.size() - 1);
-//                System.out.println("match end");
-
-            }
-
-            //if < target, do rec
-            else {
-                intList.add(value);
-                pathSumRecuration(root.getLeftNode(), target-value, intList,res);
-                pathSumRecuration(root.getRightNode(), target-value, intList,res);
-                intList.remove(intList.size()-1);
-            }
-
+        //check root is null
+        if(root == null) {
+            return;
         }
+        int value = root.getValue();
+        intList.add(value);
+        //if == target ,add to result;
+
+        if(value == target && root.getLeftNode() ==null && root.getRightNode() ==null){
+            res.add(new ArrayList(intList));
+        }
+        // do rec
+        pathSumRecuration(root.getLeftNode(), target-value, intList,res);
+        pathSumRecuration(root.getRightNode(), target-value, intList,res);
+        intList.remove(intList.size()-1);
 
     }
 
