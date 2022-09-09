f971ea5b0fd7d930258fbc91f78a1f890869008b
liubin <liubin@sensetime.com>
2022-09-09 20:16:01 +0800

    add new case

--- algorithm/src/test/java/com/strawbingo/algorithm/sword/ch2/FindNumFrom2dArrayTest.java
+++ algorithm/src/test/java/com/strawbingo/algorithm/sword/ch2/FindNumFrom2dArrayTest.java
@@ -10,11 +10,17 @@ public class FindNumFrom2dArrayTest {
                       {4,7,10,13},
                       {6,8,11,15}};
 
+    int[][] source2 = new int[0][0];
+
+    int[][] source3 = {{-5}};
+
     @Test
     public void testFindNum(){
         Assert.assertEquals(true, FindNumFrom2dArray.findNum(source,2));
         Assert.assertEquals(true, FindNumFrom2dArray.findNum(source,10));
         Assert.assertEquals(false, FindNumFrom2dArray.findNum(source,5));
         Assert.assertEquals(false, FindNumFrom2dArray.findNum(source,20));
+
+        Assert.assertEquals(true, FindNumFrom2dArray.findNum(source3,-5));
     }
 }
