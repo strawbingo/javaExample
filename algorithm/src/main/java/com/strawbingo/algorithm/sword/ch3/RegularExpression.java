c3ad0f41b5420181c942d85203730524f1ed86bd
liubin <liubin@sensetime.com>
2022-09-18 17:20:32 +0800

    modify method 2nd time

--- algorithm/src/main/java/com/strawbingo/algorithm/sword/ch3/RegularExpression.java
+++ algorithm/src/main/java/com/strawbingo/algorithm/sword/ch3/RegularExpression.java
@@ -12,95 +12,79 @@ import java.util.stream.Stream;
  */
 public class RegularExpression {
 
-    String star = "*";
-    String point = ".";
+    private char star = '*';
+    private char point = '.';
 
-    /**
-     * @param s
-     * @param p
-     * @return
-     */
     public boolean isMatch(String s, String p) {
-//        System.out.println(s+"---------" +p);
-       int m = s.length();
-       int n = p.length();
-
-       boolean[][] f = new boolean[m+1][n+1];
-       f[0][0] = true;
-       for(int i=0; i<= m ; ++i){
-           for(int j=1; j<= n; ++j){
-//               System.out.println(i+"====ij==="+j);
-               if(p.charAt(j-1) == '*'){
-                   f[i][j] = f[i][j-2];
-                   if(matches(s,p,i,j-1)){
-                       f[i][j] = f[i-1][j] || f[i][j] ;
-                   }
-               }
-               //pj is not *
-               else {
-
-                   if(matches(s,p,i,j)){
-                       f[i][j] = f[i-1][j-1];
-                   }
-               }
-           }
-       }
-
-//       Stream.of(f).forEach(t -> System.out.println(Arrays.toString(t)));
-//       System.out.println(f[m][n]);
-       return f[m][n];
-    }
-
-    private boolean matches(String s, String p, int i, int j) {
-       if(i == 0 ){
-           return false;
-       }
-
-        if(p.charAt(j-1) == '.'){
-            return true;
-        }
-
-        return s.charAt(i - 1) == p.charAt(j - 1);
-    }
-
-    public boolean isMatch2(String s, String p) {
-//        if(s.length()==0){
-//            return false;
-//        }
-
-        return matchCore(s,p);
+        //get pre char and curr char
+        return  dfs(s,p);
 
     }
 
-    public boolean matchCore(String s, String p){
-//        System.out.println(s+"---------" +p);
-        if(p.length()>=2 && p.charAt(1) == '*'){
-            if(s.length()>0 && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0)) ) {
-                return matchCore(s, p.substring(2)) || matchCore(s.substring(1),p)
-                        || matchCore(s.substring(1),p.substring(2));
+    public boolean dfs(String s, String p){
+//        System.out.println(s+":"+p);
+        if(s.length()==0 && p.length()==0) return true;
+
+        if(s.length() != 0 && p.length()==0 ) return false;
+
+
+        if(p.length()>1 ){
+            char pChar = p.charAt(0);
+            char pChar2 = p.charAt(1);
+//            System.out.println("pChar2"+pChar2);
+            //if pChar2 is * , may 0 or more than 1
+            if(pChar2 == star){
+//                System.out.println("pChar2 == star");
+                //if match
+                if(s.length()>0){
+                    if( s.charAt(0) == p.charAt(0) || pChar == point){
+                        String sSub = s.substring(1);
+                        //match 1 time
+//                        dfs(s.substring(1),p.substring(2));
+//                        // match more time;
+//                        dfs(s.substring(1),p);
+                        String pSub = p.substring(2);
+                        //match 0 time;
+                        return dfs(s,pSub) ||  dfs(sSub,p)  ;
+//                        return dfs(s.substring(1),p) || dfs(s,p.substring(2));
+                    }
+                    else {
+                        return  dfs(s, p.substring(2));
+                    }
+                }
+                else {
+                    return dfs(s,p.substring(2));
+                }
+
+//                return dfs(s,p.substring(2)) ||
+//                        ((s.length()>0 &&( s.charAt(0) == p.charAt(0) || pChar == point)) ? dfs(s.substring(1),p.substring(2)) ||dfs(s.substring(1),p): false);
             }
             else {
-                return matchCore(s,p.substring(2));
-            }
-        }
-        else if(s.length()> 0 && p.length()> 0 ){
 
-            if(s.length() == 0) return false;
+                if(s.length()>0 &&( s.charAt(0) == p.charAt(0) || pChar == point)){
+//                    System.out.println("pChar==sChar===="+s.charAt(0)+":"+pChar);
+                    return dfs(s.substring(1),p.substring(1));
+                }
+                else {
+                    return false;
+                }
+            }
 
-            if(s.charAt(0) == p.charAt(0) || p.charAt(0) == '.'){
-                return matchCore(s.substring(1),p.substring(1));
-            }
-            else {
-                return false;
-            }
         }
         else {
-            if(s.length()==p.length()) {
-                return true;
+
+            if(s.length() == 0 && p.length()!=0 ) return false;
+//            System.out.println("in p.length()<=1=="+s+":"+p);
+            if((s.length()>0 &&( s.charAt(0) == p.charAt(0) || p.charAt(0) == point))){
+                return dfs(s.substring(1),p.substring(1));
             }
-            else{
-                return false;
+            else {
+//                System.out.println("return false");
+                return  false;
             }
         }
+
+
+
     }
 }
