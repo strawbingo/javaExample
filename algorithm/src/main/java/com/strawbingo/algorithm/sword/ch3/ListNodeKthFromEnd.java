7c63d179162ba2cbbdfa166a94e32c9a324d4358
liubin <liubin@sensetime.com>
2022-09-19 01:00:24 +0800

    add comment

--- algorithm/src/main/java/com/strawbingo/algorithm/sword/ch3/ListNodeKthFromEnd.java
+++ algorithm/src/main/java/com/strawbingo/algorithm/sword/ch3/ListNodeKthFromEnd.java
@@ -5,8 +5,20 @@ import com.strawbingo.algorithm.sword.ch4.LinkNodeCopy;
 
 import java.util.Stack;
 
+/**
+ * 题22：链表中倒数第K个节点
+ * 输入一个链表，输出该链表中倒数第k个节点。
+ * 为了复合大多数人的习惯，本题从1开始计数，即链表尾节点是倒数第一个节点。
+ * 例1、2、3、4、5、6，倒数第3个节点为4节点
+ */
 public class ListNodeKthFromEnd {
 
+    /**
+     * 堆栈
+     * @param head
+     * @param k
+     * @return
+     */
     public ListNode getKthFromEnd(ListNode head, int k) {
         Stack<ListNode> listNodes = new Stack<>();
         while(head != null){
@@ -27,6 +39,12 @@ public class ListNodeKthFromEnd {
         return pop;
     }
 
+    /**
+     * 快慢指针法
+     * @param head
+     * @param k
+     * @return
+     */
     public ListNode getKthFromEndFastSlowPoint(ListNode head, int k) {
         ListNode fast = head;
         ListNode slow = head;
