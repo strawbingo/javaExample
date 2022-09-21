84a446e503049e4a20d3e41041dd0962d0a69eaa
liubin <liubin@sensetime.com>
2022-09-21 17:55:48 +0800

    modify method 2nd time

--- algorithm/src/main/java/com/strawbingo/algorithm/sword/ch4/MinStack.java
+++ algorithm/src/main/java/com/strawbingo/algorithm/sword/ch4/MinStack.java
@@ -2,30 +2,35 @@ package com.strawbingo.algorithm.sword.ch4;
 
 import java.util.Stack;
 
+/**
+ * 题30：包含min函数的栈
+ * 定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的min函数。
+ * 在该栈中，调用min，push及pop的时间复杂度为O(1)。
+ */
 public class MinStack {
-    Stack stack = new Stack();
-    Stack minStack = new Stack();
+    Stack<Integer> stack = new Stack();
+    Stack<Integer> minStack = new Stack();
 
 
-    public void push(int t) {
-        stack.push(t);
-        if( minStack.isEmpty() || t <= (Integer) minStack.peek() ){
-            minStack.push(t);
+    public void push(int x) {
+        stack.push(x);
+        if( minStack.isEmpty() || x <= minStack.peek() ){
+            minStack.push(x);
         }
     }
 
     public void pop() {
-        int pop = (int)stack.pop();
-        if(pop == (int)minStack.peek()){
+        int pop = stack.pop();
+        if(pop == minStack.peek()){
             minStack.pop();
         }
     }
 
     public int min() {
-        return (int)minStack.peek();
+        return minStack.peek();
     }
 
     public int top() {
-        return (int)stack.peek();
+        return stack.peek();
     }
 }
