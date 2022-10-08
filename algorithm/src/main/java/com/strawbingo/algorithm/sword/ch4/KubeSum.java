183a89a0f907fffec44c3b7966a3a5af2d30c081
liubin <liubin@sensetime.com>
2022-10-08 18:18:53 +0800

    modify method 2nd time

--- algorithm/src/main/java/com/strawbingo/algorithm/sword/ch4/KubeSum.java
+++ algorithm/src/main/java/com/strawbingo/algorithm/sword/ch4/KubeSum.java
@@ -1,61 +1,129 @@
 package com.strawbingo.algorithm.sword.ch4;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
- * 题38-2
- * 输入如一个含有8个数字的数组，判断有没有可能把这8个数字分别放到正方体的8个顶点上，使得正方体上三组相对面上的四个顶点的和都相等
+ * 题38-2: 8顶点
+ * 输入如一个含有8个数字的数组，判断有没有可能把这8个数字分别放到正方体的8个顶点上，
+ * 使得正方体上三组相对面上的四个顶点的和都相等
+ * https://www.freesion.com/article/2085525772/
  */
 public class KubeSum {
-    private ArrayList<String> recArr;
-    boolean[] visited;
 
     public String[] sameSum(int[] nums) {
-        int size = nums.length;
+        List<String> sum = new ArrayList();
+        List<Integer> visted = new ArrayList();
 
-        recArr = new ArrayList<>();
-        StringBuilder perm = new StringBuilder();
+        backtrack(sum,visted,nums,0);
 
-        visited = new boolean[nums.length];
-
-        backtrack(0,size,perm,nums);
-
-        ArrayList  orderList = new ArrayList<>();
-        for(int i=0; i<recArr.size(); i++){
-            //a1+a2+a3+a4=a5+a6+a7+a8 && a1+a2+a5+a6=a3+a4+a7+a8 && a1+a3+a7+a5= a2+a4+a6+a8
-            char[] chars = recArr.get(i).toCharArray();
-            if(chars[0]+chars[1]+chars[2]+chars[3] == chars[4]+chars[5]+chars[6]+chars[7]
-            && chars[0]+chars[1]+chars[4]+chars[5] == chars[2]+chars[3]+chars[6]+chars[7]
-            && chars[0]+chars[2]+chars[4]+chars[6] == chars[1]+chars[3]+chars[5]+chars[7]) {
-                orderList.add(recArr.get(i));
-            }
+        String[] ans = new String[sum.size()];
+        for (int i = 0; i < sum.size() ; i++) {
+            ans[i] = sum.get(i);
         }
-        String[] orderNums = new String[orderList.size()];
-        for(int i=0;i<orderList.size();i++){
-            orderNums[i] = orderList.get(i).toString();
-        }
-        return orderNums;
-
+        return ans;
     }
 
-    private void backtrack(int i, int size, StringBuilder perm, int[] nums) {
-        if(i == size){
-            recArr.add(perm.toString());
+    private void backtrack(List<String> sum, List<Integer> visted,int[] nums, int i) {
+        //if finish judge
+        if(i == nums.length){
+            int a1=nums[visted.get(0)], a2=nums[visted.get(1)], a3 = nums[visted.get(2)], a4 = nums[visted.get(3)],
+                    a5=nums[visted.get(4)],a6=nums[visted.get(5)],a7=nums[visted.get(6)],a8=nums[visted.get(7)];
+            if( (a1+a2+a3+a4 == a5+a6+a7+a8)&& (a1+a3+a5+a7 == a2+a4+a6+a8) && (a1+a2+a5+a6 == a3+a4+a7+a8)){
+               String tmp = a1+","+a2+","+a3+","+a4+","+a5+","+a6+","+a7+","+a8;
+               sum.add(tmp);
+            }
             return;
         }
 
-        for(int j=0; j < size; j++){
-//            System.out.println(j);
-            if(visited[j]){
+        //backtrack
+        for (int j = 0; j < nums.length; j++) {
+            if(visted.contains(j)){
                 continue;
             }
-
-            perm.append(nums[j]);
-            visited[j] = true;
-            backtrack(i+1,size,perm,nums);
-            perm.deleteCharAt(perm.length()-1);
-            visited[j] = false;
+            visted.add(j);
+            backtrack(sum,visted,nums,i+1);
+            visted.remove(visted.size()-1);
         }
     }
+
+
+    public List<String> isEightPoint(int[] num){
+        ArrayList<String> list = new ArrayList<>();
+        if (num == null)
+            return null;
+        Permutation(num,0,list);
+        return list;
+    }
+    private void Permutation(int[] num,int begin,ArrayList<String> list){
+        int end = num.length - 1;
+        if (begin == end) {
+            int a1 = num[0],a2 = num[1],a3=num[2],a4=num[3],a5 = num[4],a6 = num[5],a7 = num[6],a8 = num[7];
+            if (list.contains(Arrays.toString(num))) {
+                if ((a1 + a2 + a3 + a4 == a5 + a6 + a7 + a8) && (a1 + a3 + a5 + a7 == a2 + a4 + a6 + a8)
+                        && (a1 + a2 + a5 + a6 == a3 + a4 + a7 + a8)) {
+                    System.out.println(1);
+                    list.add(Arrays.toString(num));
+                }
+                return;
+            }
+        }
+        for(int i = begin;i<= end;i++){
+            int temp = num[begin];
+            num[begin] = num[i];
+            num[i] = temp;
+            Permutation(num,begin+1,list);
+            temp = num[begin];
+            num[begin] = num[i];
+            num[i] = temp;
+        }
+    }
+
+    private static ArrayList<String> res = new ArrayList<>();
+    /**
+     * 返回符合条件的结果集
+     * @param arr
+     * @return
+     */
+    public static ArrayList<String> isEightPoint2(int[] arr) {
+        if (arr == null || arr.length == 0 || arr.length != 8) {
+            return res;
+        }
+        permutation(arr, 0);
+        return res;
+    }
+    /**
+     * 数字的全排列
+     *
+     * @param num
+     * @param i
+     */
+    public static void permutation(int[] num, int i) {
+        if (num.length == i) {
+            int a1 = num[0], a2 = num[1], a3 = num[2], a4 = num[3],
+                    a5 = num[4], a6 = num[5], a7 = num[6], a8 = num[7];
+            //如果满足条件，则加入结果
+            if ((a1 + a2 + a3 + a4 == a5 + a6 + a7 + a8)
+                    && (a1 + a3 + a5 + a7 == a2 + a4 + a6 + a8)
+                    && (a1 + a2 + a5 + a6 == a3 + a4 + a7 + a8)) {
+                res.add(Arrays.toString(num));
+            }
+            return;
+        }
+        for (int j = i; j < num.length; j++) {
+            swap(num, i, j);
+            permutation(num, i + 1);
+            swap(num, i, j);
+        }
+
+    }
+
+    private static void swap(int[] num, int i, int j) {
+        int temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
+    }
+
 
 }
--- algorithm/src/test/java/com/strawbingo/algorithm/sword/ch4/KubeSumTest.java
+++ algorithm/src/test/java/com/strawbingo/algorithm/sword/ch4/KubeSumTest.java
@@ -3,9 +3,13 @@ package com.strawbingo.algorithm.sword.ch4;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 /**
- * 题38-2
- * 输入如一个含有8个数字的数组，判断有没有可能把这8个数字分别放到正方体的8个顶点上，使得正方体上三组相对面上的四个顶点的和都相等
+ * 题38-2: 8顶点
+ * 输入如一个含有8个数字的数组，判断有没有可能把这8个数字分别放到正方体的8个顶点上，
+ * 使得正方体上三组相对面上的四个顶点的和都相等
+ * https://www.freesion.com/article/2085525772/
  */
 public class KubeSumTest {
 
@@ -13,8 +17,11 @@ public class KubeSumTest {
     public void testSameSum(){
         KubeSum kubeSum = new KubeSum();
         int[] nums = {1,2,3,4,5,6,7,8};
+//        List<String> eightPoint = kubeSum.isEightPoint(nums);
+        List<String> eightPoint2 = kubeSum.isEightPoint2(nums);
+
         String[] orderNums = kubeSum.sameSum(nums);
 
-        Assert.assertEquals(2,orderNums.length);
+        Assert.assertEquals(144,orderNums.length);
     }
 }
