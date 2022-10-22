9fb5f9dac4ec201cc8ccc1a4225423c810522ad8
liubin <liubin@sensetime.com>
2022-10-23 02:14:18 +0800

    modify method 2nd time

--- algorithm/src/main/java/com/strawbingo/algorithm/sword/ch5/LongestSubstring.java
+++ algorithm/src/main/java/com/strawbingo/algorithm/sword/ch5/LongestSubstring.java
@@ -1,31 +1,43 @@
 package com.strawbingo.algorithm.sword.ch5;
 
-import java.util.Arrays;
+
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 题48：最长不含重复字符的子字符串
+ * 请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长字符串的长度。
+ * 假设字符串中只包含’a‘~’z‘的字符。例如，在字符串”arabcacfr“中，最长的不含重复字符的子字符串是“acfr”，长度为4.
+ */
 public class LongestSubstring {
 
+    /**
+     * 滑动窗口
+     * @param source
+     * @return
+     */
     public int lengthOfLongestSubstring(String source) {
-        int ans = 0;
         char[] chars = source.toCharArray();
-        Set<Character> charSet = new HashSet();
+        int len = chars.length;
+        int ans = 0;
         int rk = 0;
-        for (int i = 0; i < source.length(); i++) {
-            if(i != 0){
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> charSet = new HashSet<>();
+        for (int i = 0; i < len; i++) {
+            // 左指针向右移动一格，移除一个字符
+            if(i > 0){
                 charSet.remove(chars[i-1]);
             }
+            //calc rk
 
-            while (rk<source.length() && !charSet.contains(chars[rk])){
-
+            while (rk<len && !charSet.contains(chars[rk])){
+                // 不断地移动右指针
                 charSet.add(chars[rk]);
                 rk++;
-//                System.out.println("i="+i+",rk="+rk);
-//                System.out.println(Arrays.toString(charSet.toArray()));
+
             }
-
-
-            ans = Math.max(ans, rk - i);
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans,rk-i);
         }
 
         return ans;
--- algorithm/src/test/java/com/strawbingo/algorithm/sword/ch5/LongestSubstringTest.java
+++ algorithm/src/test/java/com/strawbingo/algorithm/sword/ch5/LongestSubstringTest.java
@@ -3,6 +3,11 @@ package com.strawbingo.algorithm.sword.ch5;
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * 题48：最长不含重复字符的子字符串
+ * 请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长字符串的长度。
+ * 假设字符串中只包含’a‘~’z‘的字符。例如，在字符串”arabcacfr“中，最长的不含重复字符的子字符串是“acfr”，长度为4.
+ */
 public class LongestSubstringTest {
 
     @Test
@@ -12,5 +17,6 @@ public class LongestSubstringTest {
         Assert.assertEquals(2,longestSubstring.lengthOfLongestSubstring("aab"));
         Assert.assertEquals(4,longestSubstring.lengthOfLongestSubstring("abcdbac"));
         Assert.assertEquals(2,longestSubstring.lengthOfLongestSubstring("abababab"));
+        Assert.assertEquals(3,longestSubstring.lengthOfLongestSubstring("abcabcbb"));
     }
 }
